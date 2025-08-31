c762d68e174b055460f4167ee33aef8a1e30fb52
yonatan <you@example.com>
2025-08-31 23:50:35 +0300

    hello Yonatan

--- src/simpleboot.asm
+++ src/simpleboot.asm
@@ -1,13 +1,33 @@
-mov ah, 0x0e ; tty mode
-mov al, 'H'
-int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-int 0x10 ; 'l' is still on al, remember?
-mov al, 'o'
-int 0x10
+org 0x7c00
+int 16
+
+_start:
+  mov ah, 0x0e ; tty mode
+  mov al, 'H'
+  int 0x10
+  mov al, 'e'
+  int 0x10
+  mov al, 'l'
+  int 0x10
+  int 0x10
+  mov al, 'o'
+  int 0x10
+  mov al, ' '
+  int 0x10
+  mov al, 'Y'
+  int 0x10
+  mov al, 'o'
+  int 0x10
+  mov al, 'n'
+  int 0x10
+  mov al, 'a'
+  int 0x10
+  mov al, 't'
+  int 0x10
+  mov al, 'a'
+  int 0x10
+  mov al, 'n'
+  int 0x10  
 
 jmp $ ; jump to current address = infinite loop
 
